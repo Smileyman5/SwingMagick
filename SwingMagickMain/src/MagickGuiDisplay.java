07f8064341ab13af4e8c2e36fd8bb8c3bbd1f20d
Alex Kouthoofd <akouthoo@oswego.edu>
2016-10-23 14:51:56 -0400

    Basic setup is now correct

--- SwingMagickMain/src/MagickGuiDisplay.java
+++ SwingMagickMain/src/MagickGuiDisplay.java
@@ -1,3 +1,5 @@
+import org.im4java.process.ProcessStarter;
+
 import javax.swing.*;
 
 /**
@@ -5,5 +7,17 @@ import javax.swing.*;
  */
 public class MagickGuiDisplay extends JPanel
 {
-
+    public MagickGuiDisplay()
+    {
+        try {
+            // Set cross-platform look: "Nimbus"
+            UIManager.setLookAndFeel(UIManager.getInstalledLookAndFeels()[1].getClassName());
+        }
+        catch (UnsupportedLookAndFeelException | InstantiationException
+                | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String myPath="C:\\Program Files (x86)\\ImageMagick-6.3.9-Q8";
+        ProcessStarter.setGlobalSearchPath(myPath);
+    }
 }
